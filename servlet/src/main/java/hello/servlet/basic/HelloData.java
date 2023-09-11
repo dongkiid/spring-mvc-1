81b1e1d6ff9f85abf06e73eae07691d18c9c8fde
dongkiid <mindo7211@daum.net>
2023-09-11 11:46:18 +0900

    (CH01-CH06) 서블릿, MVC 프레임워크 만들기, 스프링 MVC 기본 기능

---
+++ servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,10 @@
+package hello.servlet.basic;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class HelloData {
+    String username;
+    int age;
+}
---
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.servletmvc;
+
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Log4j2
+@WebServlet(name="mvcMemberFormServlet",urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        log.info("request = "+request);
+        log.info("response = "+response);
+        dispatcher.forward(request,response);
+    }
+}
